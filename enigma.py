49a6106a97c6510a1db99bb363322ebc705279b5
jchandd <jaydenc243@deltalearns.ca>
2026-04-12 15:10:30 -0700

    added some user stuff

--- enigma.py
+++ enigma.py
@@ -8,3 +8,14 @@
 # If encrypt → call encrypt function
 # If decrypt → call decrypt function
 # Show result
+
+print("Caesar Cipher Program")
+
+choice = input("Would you like to encrypt or decrypt? ")
+message = input("Enter your message: ")
+
+# Clean inputs
+choice = choice.strip().lower()
+message = message.strip()
+
+print("\nYou entered:", message)
