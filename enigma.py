1d3556eaf8e4fb5e50529baec5d4a400413dab20
jchandd <jaydenc243@deltalearns.ca>
2026-04-12 14:37:12 -0700

    added pseudocode

---
+++ enigma.py
@@ -0,0 +1,10 @@
+# Ceaser Cipher Program
+
+
+# PSEUDOCODE
+# Ask user to encrypt or decrypt
+# Ask for message
+# Clean message
+# If encrypt → call encrypt function
+# If decrypt → call decrypt function
+# Show result
