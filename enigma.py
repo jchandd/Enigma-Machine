6804e7e0fdbb52f07e75f54f9f8f86cf7a949337
jchandd <jaydenc243@deltalearns.ca>
2026-04-14 18:13:01 -0700

    added in final bits of code

--- enigma.py
+++ enigma.py
@@ -19,7 +19,8 @@ def format_text(text):
 
 
 def encrypt(data): # using dictionnary
-    shift = 3  # You can change this value to increase or decrease the shift
+    text = data["message"]
+    shift = data["shift"]
     result = ""
 
     for char in text:
@@ -38,34 +39,40 @@ def encrypt(data): # using dictionnary
     return result
 
 
-def decrypt(text):
-    shift = 3  # You can change this value to increase or decrease the shift
-    result = ""
+def decrypt(data): # using dictionnary
+    data["shift"] = -data["shift"] # We can simply negate the shift to decrypt, since the Caesar cipher is symmetric. This way, we can reuse the encrypt function for decryption.
+    return encrypt(data)
 
-    for char in text:
-        if char.islower():
-            new_char = chr(
-                (ord(char) - 97 - shift) % 26 + 97
-            )  # Similar to the encrypt function, but we subtract the shift instead of adding it
-            result += new_char
-        elif char.isupper():  # Check if the character is an uppercase letter
-            new_char = chr(
-                (ord(char) - 65 - shift) % 26 + 65
-            )  # Similar to the lowercase case, but we use 65 for uppercase letters
-            result += new_char
-        else:
-            result += char
-    return result
 
+
+
+
+def main():
+    choice = input("Do you want to encrypt or decrypt a message? (Type 'encrypt' or 'decrypt'): ").lower()
+    message = input("Enter your message: ")
+    shift = int(input("Enter the shift value (number of positions to shift): "))
+
+  # Clean Inputs
+choice = choice.strip().lower()
+message = format_text(message)
+
+print("\nYou entered:", message)
+
+# Code for storing data in a dictionary
+data = {
+    "message": message,
+    "shift": shift
+}
 
 if choice == "encrypt":
-    result = encrypt(message)
+    result = encrypt(data)
     print("\nEncrypted message:", result)
 elif choice == "decrypt":
-    result = decrypt(message)
+    result = decrypt(data)
     print("\nDecrypted message:", result)
 else:
-        print("Please choose either 'encrypt' or 'decrypt'.")
+    print("Please choose either 'encrypt' or 'decrypt'.")
+
 
 
 if __name__ == "__main__":
